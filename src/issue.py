83521150dac15ea25c2da366b95708f000de9850
Filipe Miranda <filipe.miranda@charliehealth.com>
2024-05-21 15:15:23 -0400

    Update script

--- src/issue.py
+++ src/issue.py
@@ -17,30 +17,39 @@ async def insert_record(connection):
                     test_col
                 ) VALUES (
                     'foo'
-                ) RETURNING *
+                ) RETURNING *;
                 """
             )
 
-            print(await cursor.fetchone())
+            record = await cursor.fetchone()
+
+            print(f"Record inserted using connection {connection}:")
+            print(record)
+
+            return record
 
 
-async def print_record(connection):
+async def print_record(connection, record_id):
     async with connection.cursor() as cursor:
         await cursor.execute(
             """
             SELECT * FROM db.test_table
-            """
+            WHERE id=%(id)s
+            """,
+            params={"id": record_id},
         )
 
-        print(await cursor.fetchall())
+        print(f"Record fetched using connection {connection}:")
+        print(await cursor.fetchone())
 
 
 async def doit():
     connection_1 = await get_connection()
     connection_2 = await get_connection()
 
-    await insert_record(connection_1)
-    await print_record(connection_2)
+    record = await insert_record(connection_1)
+
+    await print_record(connection_2, record_id=record[0])
 
 
 if __name__ == "__main__":
